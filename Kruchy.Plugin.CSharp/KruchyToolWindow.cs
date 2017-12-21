f2b9ac2dae67a9e02317f05e9ae3a592d2860e0c
kruchy <kruchy@gitlab5-vm.piatka.pl>
2017-12-21 11:26:18 +0000

    Nowy projekt dla plugina dla C#

---
+++ Kruchy.Plugin.CSharp/KruchyToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="KruchyToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Kruchy.Plugin.CSharp
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("ea1ee1f7-1058-4bba-b7f3-4f3c231e1cae")]
+    public class KruchyToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KruchyToolWindow"/> class.
+        /// </summary>
+        public KruchyToolWindow() : base(null)
+        {
+            this.Caption = "KruchyToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new KruchyToolWindowControl();
+        }
+    }
+}
---
+++ Kruchy.Plugin.CSharp/KruchyToolWindowControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="KruchyToolWindowControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Kruchy.Plugin.CSharp
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for KruchyToolWindowControl.
+    /// </summary>
+    public partial class KruchyToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KruchyToolWindowControl"/> class.
+        /// </summary>
+        public KruchyToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "KruchyToolWindow");
+        }
+    }
+}
