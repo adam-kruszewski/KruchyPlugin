ae0b2a9c8a25a5269805b121115238dd2591b3c7
kruchy <kruchy@gitlab5-vm.piatka.pl>
2017-12-21 07:56:02 +0000

    Projekt do współdzielenia utilsów odnośnie tworzenia wtyczek.

---
+++ Kruchy.Plugin.Utils/Wrappers/DokumentWrapper.cs
@@ -0,0 +1,192 @@
+﻿using System.Linq;
+using System.Text;
+using EnvDTE;
+using KrucheBuilderyKodu.Builders;
+using KruchyParserKodu.ParserKodu;
+
+namespace Kruchy.Plugin.Utils.Wrappers
+{
+    public class DokumentWrapper
+    {
+        private readonly TextDocument textDocument;
+        private readonly ProjektWrapper projektWrapper;
+
+        public DokumentWrapper(TextDocument textDocument)
+        {
+            this.textDocument = textDocument;
+        }
+
+        public DokumentWrapper(TextDocument textDocument, ProjektWrapper projekt)
+        {
+            this.textDocument = textDocument;
+            this.projektWrapper = projekt;
+        }
+
+        public void DodajUsingaJesliTrzeba(string nazwaNamespace)
+        {
+            var parsowane = Parser.Parsuj(DajZawartosc());
+
+            if (parsowane.Usingi.Select(o => o.Nazwa).Contains(nazwaNamespace))
+                return;
+
+            int wierszWstawienia = 1;
+            int kolumnaWstawienia = 1;
+            var aktualneUsingi = parsowane.Usingi.Select(o => o.Nazwa).ToList();
+            aktualneUsingi.Add(nazwaNamespace);
+
+            if (parsowane.Usingi.Any())
+            {
+                UsunWszystkieUsingi(
+                    parsowane,
+                    ref wierszWstawienia,
+                    ref kolumnaWstawienia);
+            }
+
+            var posortowaneDoWstawienia =
+                aktualneUsingi
+                    .OrderBy(o => DajKluczDoSortowaniaUsingow(o))
+                        .ToList();
+            var builder = new StringBuilder();
+            foreach (var u in posortowaneDoWstawienia)
+                builder.AppendLine("using " + u + ";");
+            var nowyTekst = builder.ToString().TrimEnd();
+
+            WstawWMiejscu(nowyTekst, wierszWstawienia, 1);
+        }
+
+        private void UsunWszystkieUsingi(
+            Plik parsowane,
+            ref int wierszWstawienia,
+            ref int kolumnaWstawienia)
+        {
+            var dotychczasowePosortowane =
+                parsowane.Usingi.OrderBy(o => o.Poczatek.Wiersz);
+
+            var pierwszyUsing = dotychczasowePosortowane.First();
+            var ostatniUsing = dotychczasowePosortowane.Last();
+
+            Usun(pierwszyUsing.Poczatek.Wiersz, pierwszyUsing.Poczatek.Kolumna,
+                ostatniUsing.Koniec.Wiersz, ostatniUsing.Koniec.Kolumna);
+            wierszWstawienia = pierwszyUsing.Poczatek.Wiersz;
+            kolumnaWstawienia = pierwszyUsing.Poczatek.Kolumna;
+        }
+
+        private string DajKluczDoSortowaniaUsingow(string nazwaUsinga)
+        {
+            if (nazwaUsinga.StartsWith("System.") || nazwaUsinga == "System")
+                return "0" + nazwaUsinga;
+            else
+                return "1" + nazwaUsinga;
+        }
+
+        public int DajNumerLiniiKursora()
+        {
+            return textDocument.Selection.TopPoint.Line;
+        }
+
+        public void UstawKursor(int wiersz, int kolumna)
+        {
+            textDocument.Selection.MoveToLineAndOffset(wiersz, kolumna);
+        }
+
+        public void UstawKursosDlaMetodyDodanejWLinii(int numerLinii)
+        {
+            UstawKursor(
+                numerLinii + 2,
+                1 + StaleDlaKodu.WciecieDlaMetody.Length
+                + StaleDlaKodu.JednostkaWciecia.Length);
+        }
+
+        public string DajZawartosc()
+        {
+            EditPoint objEditPoint =
+                (EditPoint)textDocument.StartPoint.CreateEditPoint();
+            EditPoint endEditPoint =
+                (EditPoint)textDocument.EndPoint.CreateEditPoint();
+
+            return objEditPoint.GetText(endEditPoint.AbsoluteCharOffset);
+        }
+
+        public string DajZawartoscLinii(int numerLinii)
+        {
+            var poczatekLinii = textDocument.CreateEditPoint();
+            poczatekLinii.MoveToLineAndOffset(numerLinii, 1);
+
+            var koniecLinii = textDocument.CreateEditPoint();
+            koniecLinii.MoveToLineAndOffset(numerLinii, 1);
+            koniecLinii.MoveToLineAndOffset(numerLinii, koniecLinii.LineLength + 1);
+
+            return poczatekLinii.GetText(koniecLinii);
+        }
+
+        public string DajZawartosc(int wierszPoczatek, int kolumnaPoczatek,
+            int wierszKoniec, int kolumnaKoniec)
+        {
+            var poczatekLinii = textDocument.CreateEditPoint();
+            poczatekLinii.MoveToLineAndOffset(wierszPoczatek, kolumnaPoczatek);
+
+            var koniecLinii = textDocument.CreateEditPoint();
+            koniecLinii.MoveToLineAndOffset(wierszKoniec, kolumnaKoniec);
+
+            return poczatekLinii.GetText(koniecLinii);
+        }
+
+        public void WstawWLinii(string tekst, int numerLinii)
+        {
+            var poczatekLinii =
+            DajEditPointPoczatkuLinii(numerLinii);
+            poczatekLinii.Insert(tekst);
+        }
+
+        public void WstawWMiejscu(string tekst, int numerLinii, int numerKolumny)
+        {
+            var editPoint = DajEditPointPoczatkuLinii(numerLinii);
+            editPoint.MoveToLineAndOffset(numerLinii, numerKolumny);
+            editPoint.Insert(tekst);
+        }
+
+        public void UsunWMiejscu(int numerLinii, int numerKolumny, int dlugosc)
+        {
+            var editPoint = DajEditPointMiejsca(numerLinii, numerKolumny);
+            var editPointKoniec = DajEditPointMiejsca(numerLinii, numerKolumny + dlugosc);
+            editPoint.Delete(editPointKoniec);
+        }
+
+        public void Usun(int numerLiniiStart, int numerKolumnyStart,
+            int numerLiniiKoniec, int numerKolumnyKoniec)
+        {
+            var editPoint = DajEditPointMiejsca(numerLiniiStart, numerKolumnyStart);
+            var editPointKoniec = DajEditPointMiejsca(numerLiniiKoniec, numerKolumnyKoniec);
+            editPoint.Delete(editPointKoniec);
+        }
+
+        public void UsunLinie(int numerLinii)
+        {
+            var editPoint = DajEditPointPoczatkuLinii(numerLinii);
+            var editPointKonca = DajEditPointPoczatkuLinii(numerLinii);
+            editPointKonca.LineDown();
+            editPoint.Delete(editPointKonca);
+        }
+
+        private EditPoint DajEditPointPoczatkuLinii(
+            int numerLinii)
+        {
+            var poczatekLinii = textDocument.CreateEditPoint();
+            poczatekLinii.MoveToLineAndOffset(numerLinii, 1);
+            return poczatekLinii;
+        }
+
+        private EditPoint DajEditPointMiejsca(
+            int numerLinii, int numerKolumny)
+        {
+            var editPoint = DajEditPointPoczatkuLinii(numerLinii);
+            editPoint.MoveToLineAndOffset(numerLinii, numerKolumny);
+            return editPoint;
+        }
+
+        public int DajLiczbeLinii()
+        {
+            return textDocument.EndPoint.Line;
+        }
+    }
+}
---
+++ Kruchy.Plugin.Utils/Wrappers/SolutionWrapper.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EnvDTE;
+using EnvDTE80;
+
+namespace Kruchy.Plugin.Utils.Wrappers
+{
+    public class SolutionWrapper
+    {
+        private readonly DTE2 dte;
+
+        public SolutionWrapper(DTE2 dte)
+        {
+            this.dte = dte;
+        }
+
+        public string PelnaNazwa
+        {
+            get { return dte.Solution.FullName; }
+        }
+
+        public string Nazwa
+        {
+            get
+            {
+                var fi = new FileInfo(PelnaNazwa);
+                return fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
+            }
+        }
+
+        public string Katalog
+        {
+            get
+            {
+                var fi = new FileInfo(PelnaNazwa);
+                return fi.DirectoryName;
+            }
+        }
+
+        public PlikWrapper AktualnyPlik
+        {
+            get { return new PlikWrapper(dte.ActiveDocument); }
+        }
+
+        public ProjektWrapper AktualnyProjekt
+        {
+            get
+            {
+                var plik = AktualnyPlik;
+                if (plik == null)
+                    return null;
+
+                return plik.Projekt;
+            }
+        }
+
+        public DokumentWrapper AktualnyDokument
+        {
+            get
+            {
+                if (dte.ActiveDocument == null)
+                    return null;
+
+                var textDoc = (TextDocument)dte.ActiveDocument.Object("TextDocument");
+                return new DokumentWrapper(textDoc);
+            }
+        }
+
+        public DTE2 DTE { get { return dte; } }
+
+        public IList<ProjektWrapper> Projekty
+        {
+            get
+            {
+                var wynik = new List<ProjektWrapper>();
+                var solution = dte.Solution;
+                for (int i = 1; i <= solution.Count; i++)
+                {
+                    var p = solution.Projects.Item(i);
+                    if (p.FullName.ToLower().EndsWith(".csproj"))
+                        wynik.Add(new ProjektWrapper(p));
+                    else
+                        SzukajProjektow(p, wynik);
+                }
+                return wynik;
+            }
+        }
+
+        private void SzukajProjektow(Project p, List<ProjektWrapper> wynik)
+        {
+            for (int i = 1; i <= p.ProjectItems.Count; i++)
+            {
+                var item = p.ProjectItems.Item(i);
+                SzukajProjektowWProjectItem(item, wynik);
+            }
+        }
+
+        private void SzukajProjektowWProjectItem(
+            ProjectItem pi,
+            List<ProjektWrapper> wynik)
+        {
+            var itemObject = pi.Object as Project;
+            if (itemObject == null)
+                return;
+            if (itemObject.FullName.ToLower().EndsWith(".csproj"))
+                wynik.Add(new ProjektWrapper(itemObject));
+            else
+            {
+                for (int i = 1; i <= itemObject.ProjectItems.Count; i++)
+                {
+                    var item = itemObject.ProjectItems.Item(i);
+                    SzukajProjektowWProjectItem(item, wynik);
+                }
+            }
+        }
+
+        public ProjektWrapper ZnajdzProjktDlaPliku(string nazwa)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ProjektWrapper ZnajdzProjekt(string nazwa)
+        {
+            var l = Projekty.ToList();
+            return l.Where(o => o.Nazwa == nazwa).FirstOrDefault();
+        }
+
+        public void OtworzPlik(string sciezka)
+        {
+            new SolutionExplorerWrapper(this).OtworzPlik(sciezka);
+        }
+    }
+}
