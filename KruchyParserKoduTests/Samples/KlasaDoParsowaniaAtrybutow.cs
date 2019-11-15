9880318280b16d1edafd5529f4ca9e70f32a8fa5
kruchy <kruchy@piatka.pl>
2019-11-15 08:25:32 +0100

    Dodaję testy parsowania

---
+++ KruchyParserKoduTests/Samples/KlasaDoParsowaniaAtrybutow.cs
@@ -0,0 +1,33 @@
+﻿
+namespace KruchyCompany.KruchyPlugin1Tests.ParserTests
+{
+    [Testowo]
+    [Map(typeof(KlasaDoParsowaniaAtrybutow))]
+    class KlasaDoParsowaniaAtrybutow
+    {
+        [Testowo, Testowo3]
+        public void Metoda1()
+        {
+
+        }
+
+        [Testowo2(Param = "aa")]
+        [Testowo3]
+        public void Metoda2()
+        {
+
+        }
+
+        [Testowo2(Param = "bb"), Testowo]
+        public void Metoda3()
+        {
+
+        }
+
+        [Testowo4(1)]
+        public void Metoda4()
+        {
+
+        }
+    }
+}
---
+++ KruchyParserKoduTests/Unit/ParsowanieKlasyNadklasIInterfejsowTests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using KruchyParserKodu.ParserKodu;
+using KruchyParserKoduTests.Utils;
+using NUnit.Framework;
+
+namespace KruchyParserKoduTests.Unit
+{
+    [TestFixture]
+    public class ParsowanieKlasyNadklasIInterfejsowTests
+    {
+        [Test]
+        public void PoprawnieParsuje()
+        {
+            //arrange
+            var zawartosc =
+                new WczytywaczZawartosciPrzykladow()
+                    .DajZawartoscPrzykladu("KlasaDoParsowaniaNadklasIInterfejsow.cs");
+
+            //act
+            var plik = Parser.Parsuj(zawartosc);
+
+            //assert
+            var klasa = plik.DefiniowaneObiekty.First();
+
+            klasa.NadklasaIInterfejsy.Count().Should().Be(3);
+            klasa.NadklasaIInterfejsy.First().Nazwa.Should().Be("Test3");
+            klasa.NadklasaIInterfejsy[1].Nazwa.Should().Be("ITest1");
+            klasa.NadklasaIInterfejsy[2].Nazwa.Should().Be("ITest2");
+            klasa.NadklasaIInterfejsy[2].Poczatek.Sprawdz(9, 65);
+            klasa.NadklasaIInterfejsy[2].Koniec.Sprawdz(9, 71);
+        }
+    }
+}
