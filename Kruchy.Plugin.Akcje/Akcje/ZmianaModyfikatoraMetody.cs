e697dce3886909b81123416000d169fe40f6029a
kruchy <kruchy@piatka.pl>
2020-01-17 12:38:36 +0100

    Zmiana modyfikatora będzie potrafiło zmienić modyfikator klasy, jeśli nie stoi się w metodzie #15

--- Kruchy.Plugin.Akcje/Akcje/ZmianaModyfikatoraMetody.cs
+++ Kruchy.Plugin.Akcje/Akcje/ZmianaModyfikatoraMetody.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Kruchy.Plugin.Utils.Wrappers;
 using KruchyParserKodu.ParserKodu;
 
@@ -25,14 +27,38 @@ namespace Kruchy.Plugin.Akcje.Akcje
                 if (metoda.ZawieraModyfikator(modyfikator))
                     return;
 
-                var dotychczasowyModyfikator =
-                    SzukajDotychczasowegoModyfikatora(metoda);
-
-                if (dotychczasowyModyfikator == null)
-                    WstawModyfikator(modyfikator, metoda);
-                else
-                    ZmienModyfikator(modyfikator, dotychczasowyModyfikator);
+                ZmienWMetodzie(modyfikator, metoda);
+            }else
+            {
+                var klasa = parsowane.SzukajKlasyWLinii(dokument.DajNumerLiniiKursora());
+                if (klasa != null)
+                    ZmienWKlasie(modyfikator, klasa);
             }
+        }
+
+        private void ZmienWKlasie(string modyfikator, Obiekt klasa)
+        {
+            var dotychczasowyModyfikator =
+                SzukajDotychczasowegoModyfikatora(klasa.Modyfikatory);
+
+            if (klasa.Wlasciciel == null && modyfikator == "private")
+                modyfikator = "";
+
+            if (dotychczasowyModyfikator == null)
+                WstawModyfikator(modyfikator, klasa.Poczatek);
+            else
+                ZmienModyfikator(modyfikator, dotychczasowyModyfikator);
+        }
+
+        private void ZmienWMetodzie(string modyfikator, Metoda metoda)
+        {
+            var dotychczasowyModyfikator =
+                SzukajDotychczasowegoModyfikatora(metoda.Modyfikatory);
+
+            if (dotychczasowyModyfikator == null)
+                WstawModyfikator(modyfikator, metoda.Poczatek);
+            else
+                ZmienModyfikator(modyfikator, dotychczasowyModyfikator);
         }
 
         private void ZmienModyfikator(
@@ -50,20 +76,22 @@ namespace Kruchy.Plugin.Akcje.Akcje
                 dotychczasowyModyfikator.Poczatek.Kolumna);
         }
 
-        private void WstawModyfikator(string modyfikator, Metoda metoda)
+        private void WstawModyfikator(string modyfikator, PozycjaWPliku polozenie)
         {
             dokument.WstawWMiejscu(
                 modyfikator + " ",
-                metoda.Poczatek.Wiersz,
-                metoda.Poczatek.Kolumna);
+                polozenie.Wiersz,
+                polozenie.Kolumna);
         }
 
-        private Modyfikator SzukajDotychczasowegoModyfikatora(Metoda metoda)
+        private Modyfikator SzukajDotychczasowegoModyfikatora(
+            IEnumerable<Modyfikator> aktualneModyfikatory)
         {
             var dotychczasowyModyfikator =
-                metoda.Modyfikatory
+                aktualneModyfikatory
                     .Where(o => modyfikatory.Any(m => m == o.Nazwa))
                         .FirstOrDefault();
+
             return dotychczasowyModyfikator;
         }
     }
