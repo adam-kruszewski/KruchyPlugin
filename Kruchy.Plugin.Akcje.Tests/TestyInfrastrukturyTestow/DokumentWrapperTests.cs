8f9bd2b1155c0c0938eca41fff0084b2628a8be0
kruchy <kruchy@piatka.pl>
2020-01-23 08:30:41 +0100

    Naprawa testowej implementacji dokument wrappera #10

--- Kruchy.Plugin.Akcje.Tests/TestyInfrastrukturyTestow/DokumentWrapperTests.cs
+++ Kruchy.Plugin.Akcje.Tests/TestyInfrastrukturyTestow/DokumentWrapperTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
 using Kruchy.Plugin.Akcje.Tests.Utils;
 using Kruchy.Plugin.Akcje.Tests.WrappersMocks;
 using Kruchy.Plugin.Utils.Wrappers;
@@ -24,7 +25,7 @@ namespace Kruchy.Plugin.Akcje.Tests.TestyInfrastrukturyTestow
         public void WstawiaWLinii()
         {
             //act
-            dokument.WstawWMiejscu("abc", 1, 0);
+            dokument.WstawWMiejscu("abc", 1, 1);
 
             //assert
             dokument.DajZawartosc()
@@ -33,6 +34,185 @@ namespace Kruchy.Plugin.Akcje.Tests.TestyInfrastrukturyTestow
 @"abcnamespace Kruchy.Plugin.Akcje.Tests.Samples
 {
     class PustaKlasa
+    {
+    }
+}");
+        }
+
+        [Test]
+        public void WstawiaWLiniiTeskstZEnterem()
+        {
+            //act
+            dokument.WstawWMiejscu("abc" + new StringBuilder().AppendLine().ToString(), 1, 1);
+
+            //assert
+            dokument.DajZawartosc()
+                .Should()
+                    .Be(
+@"abc
+namespace Kruchy.Plugin.Akcje.Tests.Samples
+{
+    class PustaKlasa
+    {
+    }
+}");
+        }
+
+        [Test]
+        public void WstawiaWSrodkuLinii()
+        {
+            //act
+            dokument.WstawWMiejscu("abc", 1, 3);
+
+            //assert
+            dokument.DajZawartosc()
+                .Should()
+                    .Be(
+@"naabcmespace Kruchy.Plugin.Akcje.Tests.Samples
+{
+    class PustaKlasa
+    {
+    }
+}");
+        }
+
+        [Test]
+        public void UsuwaUsingi()
+        {
+            //arrange
+            var zrodlo =
+@"using a.b.c;
+using c.d.e;
+using g.h.i;
+
+namespace x.y.z{
+  class A {}
+}";
+
+            var dokument = new DokumentWrapper(zrodlo);
+
+            //act
+            dokument.Usun(1, 1, 3, 13);
+
+            //assert
+            dokument.DajZawartosc().Should().Be(
+@"
+
+namespace x.y.z{
+  class A {}
+}");
+        }
+
+        [Test]
+        public void UsuwaTekstZLinii()
+        {
+            //arrange
+            var zrodlo =
+@"using a.b.c;
+using c.d.e;
+using g.h.i;
+
+namespace x.y.z{
+  class A {}
+}";
+
+            var dokument = new DokumentWrapper(zrodlo);
+
+            //act
+            dokument.Usun(1, 2, 1, 4);
+
+            //assert
+            dokument.DajZawartosc().Should().Be(
+@"ug a.b.c;
+using c.d.e;
+using g.h.i;
+
+namespace x.y.z{
+  class A {}
+}");
+        }
+
+        [Test]
+        public void UsuwaTekstZPoczatkuLinii()
+        {
+            //arrange
+            var zrodlo =
+@"using a.b.c;
+using c.d.e;";
+
+            var dokument = new DokumentWrapper(zrodlo);
+
+            //act
+            dokument.Usun(1, 1, 1, 4);
+
+            //assert
+            dokument.DajZawartosc().Should().Be(
+@"g a.b.c;
+using c.d.e;");
+        }
+
+        [Test]
+        public void NieUsuwaKoncaLiniiJesliPodanyKoncowyIndeksSlashR()
+        {
+            //arrange
+            var zrodlo =
+@"using a.b.c;
+using c.d.e;";
+
+            var dokument = new DokumentWrapper(zrodlo);
+
+            //act
+            dokument.Usun(1, 1, 1, 13);
+
+            //assert
+            dokument.DajZawartosc().Should().Be(
+@"
+using c.d.e;");
+        }
+
+        [Test]
+        public void UsuwaCalaLinieJesliPodanyKoncowyIndeksSlashN()
+        {
+            //arrange
+            var zrodlo =
+@"using a.b.c;
+using c.d.e;";
+
+            var dokument = new DokumentWrapper(zrodlo);
+
+            //act
+            dokument.Usun(1, 1, 1, 14);
+
+            //assert
+            dokument.DajZawartosc().Should().Be(
+@"using c.d.e;");
+        }
+
+        [Test]
+        public void UsuwaWielolinijkowe()
+        {
+            //arrange
+            var zrodlo =
+@"using a.b.c;
+using b.c.d;
+
+namespace X{
+    class a
+    {
+    }
+}";
+
+            var dokument = new DokumentWrapper(zrodlo);
+
+            //act
+            dokument.Usun(1, 1, 2, 13);
+
+            //assert
+            dokument.DajZawartosc().Should().Be(
+@"
+
+namespace X{
+    class a
     {
     }
 }");
