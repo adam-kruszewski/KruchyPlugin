506c1e8e2cceb312db8a6ef3f78511024568a40b
Adam Kruszewski <kruchy.jr@wp.pl>
2021-07-25 21:42:15 +0200

    dodanie rozszerzenia wydzielającego szukanie klasy testowanej

---
+++ Kruchy.Plugin.Akcje/Utils/KlasaTestowanaExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Kruchy.Plugin.Utils.Extensions;
+using Kruchy.Plugin.Utils.Wrappers;
+using System.Linq;
+
+namespace Kruchy.Plugin.Akcje.Utils
+{
+    public static class KlasaTestowanaExtensions
+    {
+        public static IPlikWrapper SzukajPlikuKlasyTestowanej(this ISolutionWrapper solution)
+        {
+            var projektModulu =
+                solution.SzukajProjektuModulu(solution.AktualnyProjekt);
+
+            if (projektModulu == null)
+            {
+                System.Windows.MessageBox.Show("Nie znaleziono projektu modułu");
+                return null;
+            }
+
+            var nazwaSzukanegoPliku =
+                solution.AktualnyPlik.NazwaBezRozszerzenia.ToLower()
+                .Replace("tests", "");
+            var plik = SzukajPlikuKlasyTestowanej(projektModulu, nazwaSzukanegoPliku);
+
+            return plik;
+        }
+
+        private static IPlikWrapper SzukajPlikuKlasyTestowanej(
+            IProjektWrapper projektModulu,
+            string nazwaSzukanegoPliku)
+        {
+            return projektModulu
+                    .Pliki
+                        .Where(o => o.NazwaBezRozszerzenia.ToLower() == nazwaSzukanegoPliku.ToLower())
+                            .FirstOrDefault();
+        }
+    }
+}
