511751bba3bc68785b9bd59eb0fee002e1c266b8
Adam Kruszewski <kruchy.jr@wp.pl>
2021-08-09 21:38:30 +0200

    dodanie kruchego pluginu 2017

---
+++ src/Kruchy.Plugin.Utils.2017/PozycjaMenuAdapter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Kruchy.Plugin.Utils.Menu;
+using Kruchy.Plugin.Utils.Wrappers;
+using Microsoft.VisualStudio.Shell;
+
+namespace Kruchy.Plugin.Utils._2017
+{
+    public class PozycjaMenuAdapter
+    {
+        public static Guid guidKruchyPluginCmdSetStatic;
+
+        private readonly IPozycjaMenu pozycjaMenu;
+        private readonly ISolutionWrapper solution;
+
+        OleMenuCommand MenuItem { get; set; }
+
+        public PozycjaMenuAdapter(IPozycjaMenu pozycjaMenu, ISolutionWrapper solution)
+        {
+            this.pozycjaMenu = pozycjaMenu;
+            this.solution = solution;
+        }
+
+        public void Podlacz(IMenuCommandService service)
+        {
+            Podlacz(service, guidKruchyPluginCmdSetStatic);
+        }
+
+        public void Podlacz(IMenuCommandService service, Guid guidKruchyPluginCmdSet)
+        {
+            var menuCommandID =
+                new CommandID(guidKruchyPluginCmdSet, (int)pozycjaMenu.MenuCommandID);
+            if (!(pozycjaMenu is IPozycjaMenuDynamicznieRozwijane))
+                MenuItem = new OleMenuCommand(Execute, menuCommandID);
+            else
+                MenuItem = new DynamicItemMenuCommand(
+                    menuCommandID, (IPozycjaMenuDynamicznieRozwijane)pozycjaMenu);
+            MenuItem.BeforeQueryStatus += BeforeQueryStatus;
+            service.AddCommand(MenuItem);
+        }
+
+        void BeforeQueryStatus(object sender, EventArgs e)
+        {
+            if (!SpelnioneWymagania())
+            {
+                this.MenuItem.Enabled = false;
+            }
+            else
+            {
+                this.MenuItem.Enabled = true;
+            }
+
+            if (SpecyficznaDlaPincasso())
+            {
+                this.MenuItem.Visible = false;
+                this.MenuItem.Enabled = false;
+            }
+        }
+
+        private bool SpecyficznaDlaPincasso()
+        {
+            var customAttributes =
+                pozycjaMenu
+                    .GetType()
+                        .GetCustomAttributes();
+
+            if (customAttributes
+                .Where(o => o.GetType().Name == "SpecyficzneDlaPincassoAttribute")
+                    .Any())
+                return true;
+            return false;
+        }
+
+        private bool SpelnioneWymagania()
+        {
+            return pozycjaMenu.SpelnioneWymaganie(solution);
+        }
+
+        public void Execute(object sender, EventArgs args)
+        {
+            try
+            {
+                pozycjaMenu.Execute(sender, args);
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ src/Kruchy.Plugin.Utils.2017/Wrappers/PlikWrapper.cs
@@ -0,0 +1,111 @@
+﻿using System.IO;
+using EnvDTE;
+using Kruchy.Plugin.Utils.Wrappers;
+
+namespace Kruchy.Plugin.Utils._2017.Wrappers
+{
+#pragma warning disable VSTHRD010
+    public class PlikWrapper : IPlikWrapper
+    {
+        private Document document;
+        private ProjectItem projectItem;
+
+        public PlikWrapper(EnvDTE.Document document)
+        {
+            this.document = document;
+        }
+
+        public PlikWrapper(ProjectItem projectItem)
+        {
+            this.projectItem = projectItem;
+        }
+
+        public string Nazwa
+        {
+            get
+            {
+                FileInfo fi;
+                if (document != null)
+                    fi = new FileInfo(document.FullName);
+                else
+                    fi = new FileInfo(projectItem.FileNames[0]);
+                return fi.Name;
+            }
+        }
+
+        public string NazwaBezRozszerzenia
+        {
+            get
+            {
+                var index = Nazwa.LastIndexOf('.');
+                if (index < 0)
+                    return Nazwa;
+                else
+                    return Nazwa.Substring(0, index);
+            }
+        }
+
+        public string SciezkaPelna
+        {
+            get
+            {
+                if (document != null)
+                    return document.FullName;
+                else
+                {
+                    var fi = new FileInfo(projectItem.FileNames[0]);
+                    return fi.FullName;
+                }
+            }
+        }
+
+        public string Katalog
+        {
+            get
+            {
+                var fi = new FileInfo(SciezkaPelna);
+                return fi.DirectoryName;
+            }
+        }
+
+        public string SciezkaWzgledna
+        {
+            get
+            {
+                var p = SciezkaPelna;
+
+                var katalogProjektu = Projekt.SciezkaDoKatalogu;
+                p = p.Replace(katalogProjektu, "");
+                return p;
+            }
+        }
+
+        public IProjektWrapper Projekt
+        {
+            get
+            {
+                if (projectItem != null)
+                    return new ProjektWrapper(projectItem.ContainingProject);
+                else
+                    if (document.ProjectItem != null)
+                        return new ProjektWrapper(document.ProjectItem.ContainingProject);
+
+                return null;
+            }
+        }
+
+        public IDokumentWrapper Dokument
+        {
+            get
+            {
+                var textDocument = (TextDocument)document.Object("TextDocument");
+                return new DokumentWrapper(textDocument);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} [{1}]", Nazwa, Katalog);
+        }
+    }
+}
