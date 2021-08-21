c5de7b0354497d340fce9de4056a404409017da9
Adam Kruszewski <kruchy.jr@wp.pl>
2021-08-21 21:31:24 +0200

    rozszerzenie menu bez podłączania akcji jeszcze

--- src/Kruchy.Plugin.Akcje/Akcje/UzupelnianieDokumentacji.cs
+++ src/Kruchy.Plugin.Akcje/Akcje/UzupelnianieDokumentacji.cs
@@ -66,8 +66,6 @@ namespace Kruchy.Plugin.Akcje.Akcje
 
                 GennerujSummary(poczatek, builder, GenerujSummaryKlasyLubInterfejsu(obiekt));
 
-                if (obiekt.)
-
                 var doWstawienia = builder.ToString();
 
                 var numerLinii = obiekt.Poczatek.Wiersz;
