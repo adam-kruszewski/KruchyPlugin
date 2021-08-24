7f20448261863840855bf89149db30175e54d40b
Adam Kruszewski <kruchy.jr@wp.pl>
2021-08-24 21:01:01 +0200

    Usunięcie z ogólnej wtyczki opcji z pincasso

--- src/KruchyPlugin2019/Menu/PozycjaDodawanieMapowan.cs
+++ src/KruchyPlugin2019/Menu/PozycjaDodawanieMapowan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Kruchy.Plugin.Akcje.Atrybuty;
 using Kruchy.Plugin.Akcje.Menu;
 using Kruchy.Plugin.Utils.Menu;
 using Kruchy.Plugin.Utils.Wrappers;
@@ -7,7 +8,8 @@ using KruchyCompany.KruchyPlugin1.Akcje;
 
 namespace KruchyCompany.KruchyPlugin1.Menu
 {
-    class PozycjaDodawanieMapowan : IPozycjaMenu
+    [SpecyficzneDlaPincasso]
+    public class PozycjaDodawanieMapowan : IPozycjaMenu
     {
         private readonly ISolutionWrapper solution;
 
