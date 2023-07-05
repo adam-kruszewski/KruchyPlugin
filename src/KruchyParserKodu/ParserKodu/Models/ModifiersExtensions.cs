6021b1ca499c62097b93829ef75d3b3d193ae1e8
Barcelona2\Adam <kruchy.jr@wp.pl>
2023-07-05 21:45:32 +0200

    moved and translated extensions for modifier

---
+++ src/KruchyParserKodu/ParserKodu/Models/ModifiersExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KruchyParserKodu.ParserKodu.Models
+{
+    public static class ModifiersExtensions
+    {
+        public static bool ContainsModifier(
+            this IEnumerable<Modifier> modifiers,
+            string searched)
+        {
+            return modifiers.Any(o => o.Name == searched);
+        }
+    }
+}
